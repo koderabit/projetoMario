8176e304625fe200fae938ae097e09deb5bed003
KoderaBit <123130797+koderabit@users.noreply.github.com>
2023-02-06 11:49:46 -0300

    update game-over
    Feita a inclusão da mensagem de game over, junto à paralisação das nuvens neste instante;

--- script/script.js
+++ script/script.js
@@ -1,5 +1,8 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
+const gameOver = document.querySelector('.gameOver');
+
 const jump = () => {
     mario.classList.add('jump');
     setTimeout(() => {
@@ -10,6 +13,7 @@ const jump = () => {
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const cloudsPosition = +window.getComputedStyle(clouds).left.replace('px', '');
 
     if(pipePosition <= 120 && marioPosition < 80 && pipePosition > 0) {
         pipe.style.animation = 'none';
@@ -18,9 +22,13 @@ const loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.left = `${marioPosition}px`;
 
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
+
         mario.src = './image/game-over.png';
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
+        gameOver.style.display = 'block';
 
         clearInterval(loop);
     }
